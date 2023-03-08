5ca222701f7b35a6286730090ce94c99583c33b2
2236577 <2236577@SVPHLP00801>
2023-03-08 14:22:46 +0800

    Http Excercise -PUT Method by Shiena Dale Nucup

---
+++ RestfulAPITest_HTTP_Method/PetModel.cs
@@ -0,0 +1,40 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestfulAPITest_HTTP_Method
+{
+    public class PetModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("category")]
+        public Category Category { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("photoUrls")]
+        public string[] PhotoUrls { get; set; }
+
+        [JsonProperty("tags")]
+        public Category[] Tags { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+
+    public partial class Category
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+
+}
